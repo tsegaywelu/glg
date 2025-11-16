871caf27f56279421799d3cab83456bed40145e4
tsegay <tsegaygebrezgi56@gmail.com>
2025-11-16 10:32:16 +0300

    the i18n.ts working now

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "../locales/en.json";
 import ko from "../locales/ko.json";
-
+//lets push inorder to work
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
